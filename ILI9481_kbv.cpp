835fa49baa171c4d092fff1bdd26ce4c5d9eef22
prenticedavid <david.prentice@farming.co.uk>
2016-05-10 23:03:35 +0100

    first commit

---
+++ ILI9481_kbv.cpp
@@ -0,0 +1,329 @@
+#include "ILI9481_kbv.h"
+#include "serial9_kbv.h"
+
+ILI9481_kbv::ILI9481_kbv():Adafruit_GFX(320, 480)
+{
+/*
+    INIT();
+    CS_IDLE;
+    RESET_IDLE;
+*/
+}
+
+void ILI9481_kbv::reset(void)
+{
+    INIT();
+    CS_IDLE;
+    RESET_IDLE;
+	wait_ms(50);
+	RESET_ACTIVE;
+	wait_ms(100);
+	RESET_IDLE;
+	wait_ms(100);
+}
+
+void ILI9481_kbv::pushCommand(uint16_t cmd, uint8_t * block, int8_t N)
+{
+    uint16_t color;
+    CS_ACTIVE;
+    WriteCmd(cmd);
+	write9_block(block, N);
+    CS_IDLE;
+}
+
+#define ILI9481_CMD_NOP                             0x00
+#define ILI9481_CMD_SOFTWARE_RESET                  0x01
+#define ILI9481_CMD_READ_RED                        0x06
+#define ILI9481_CMD_READ_GREEN                      0x07
+#define ILI9481_CMD_READ_BLUE                       0x08
+#define ILI9481_CMD_READ_DISP_POWER                 0x0A
+#define ILI9481_CMD_READ_DISP_MADCTRL               0x0B
+#define ILI9481_CMD_READ_DISP_PIXEL_FORMAT          0x0C
+#define ILI9481_CMD_READ_DISP_IMAGE_FORMAT          0x0D
+#define ILI9481_CMD_READ_DISP_SIGNAL_MODE           0x0E
+#define ILI9481_CMD_READ_DISP_SELF_DIAGNOSTIC       0x0F
+#define ILI9481_CMD_ENTER_SLEEP_MODE                0x10
+#define ILI9481_CMD_SLEEP_OUT                       0x11
+#define ILI9481_CMD_PARTIAL_MODE_ON                 0x12
+#define ILI9481_CMD_NORMAL_DISP_MODE_ON             0x13
+#define ILI9481_CMD_DISP_INVERSION_OFF              0x20
+#define ILI9481_CMD_DISP_INVERSION_ON               0x21
+#define ILI9481_CMD_GAMMA_SET                       0x26
+#define ILI9481_CMD_DISPLAY_OFF                     0x28
+#define ILI9481_CMD_DISPLAY_ON                      0x29
+#define ILI9481_CMD_COLUMN_ADDRESS_SET              0x2A
+#define ILI9481_CMD_PAGE_ADDRESS_SET                0x2B
+#define ILI9481_CMD_MEMORY_WRITE                    0x2C
+#define ILI9481_CMD_COLOR_SET                       0x2D
+#define ILI9481_CMD_MEMORY_READ                     0x2E
+#define ILI9481_CMD_PARTIAL_AREA                    0x30
+#define ILI9481_CMD_VERT_SCROLL_DEFINITION          0x33
+#define ILI9481_CMD_TEARING_EFFECT_LINE_OFF         0x34
+#define ILI9481_CMD_TEARING_EFFECT_LINE_ON          0x35
+#define ILI9481_CMD_MEMORY_ACCESS_CONTROL           0x36
+#define ILI9481_CMD_VERT_SCROLL_START_ADDRESS       0x37
+#define ILI9481_CMD_IDLE_MODE_OFF                   0x38
+#define ILI9481_CMD_IDLE_MODE_ON                    0x39
+#define ILI9481_CMD_COLMOD_PIXEL_FORMAT_SET         0x3A
+#define ILI9481_CMD_WRITE_MEMORY_CONTINUE           0x3C
+#define ILI9481_CMD_READ_MEMORY_CONTINUE            0x3E
+#define ILI9481_CMD_SET_TEAR_SCANLINE               0x44
+#define ILI9481_CMD_GET_SCANLINE                    0x45
+#define ILI9481_CMD_COMMAND_ACCESS                  0xB0
+#define ILI9481_CMD_READ_ID                         0xBF
+
+uint16_t ILI9481_kbv::readReg(uint16_t reg)
+{
+    uint8_t h, l;
+    CS_ACTIVE;
+    WriteCmd(reg);
+    CD_DATA;                    //should do a flush()
+
+    // needs 1 dummy read
+    //     h = xchg8(0);    
+    h = xchg8(0xFF);
+    l = xchg8(0xFF);
+    CS_IDLE;
+    return (h << 8) | l;
+}
+
+uint32_t ILI9481_kbv::readReg32(uint16_t reg)
+{
+	uint32_t ret = 0;
+	CS_ACTIVE;
+	WriteCmd(reg);
+	CD_DATA;                    //should do a flush()
+    
+	readbits(1);
+    for (uint8_t cnt = 5; cnt--; ) {
+		ret <<= 8;
+		ret |= xchg8(0);
+	}
+	CS_IDLE;
+	return ret;
+}
+
+int16_t ILI9481_kbv::readGRAM(int16_t x, int16_t y, uint16_t * block, int16_t w, int16_t h)
+{
+    uint8_t r, g, b;
+	  int16_t n = w * h;    // we are NEVER going to read > 32k pixels at once
+    setAddrWindow(x, y, x + w - 1, y + h - 1);
+    CS_ACTIVE;
+    WriteCmd(ILI9481_CMD_MEMORY_READ);
+    CD_DATA;
+
+    // needs 1 dummy read
+//    r = xchg8(0xFF);
+    r = readbits(9);
+    while (n-- > 0) {
+        b = xchg8(0xFF);
+        g = xchg8(0xFF);
+        r = xchg8(0xFF);
+		*block++ = color565(r, g, b);
+    }
+    CS_IDLE;
+    setAddrWindow(0, 0, width() - 1, height() - 1);
+    return 0;
+}
+
+void ILI9481_kbv::setRotation(uint8_t r)
+{
+    uint8_t mac = 0x00;
+    Adafruit_GFX::setRotation(r & 3);
+    switch (rotation) {
+    case 0:
+        mac = 0x08;
+        break;
+    case 1:        //LANDSCAPE 90 degrees 
+        mac = 0x29;
+        break;
+    case 2:
+        mac = 0x0B;
+        break;
+    case 3:
+        mac = 0x2A;
+        break;
+    }
+	mac ^= (_lcd_xor);
+    pushCommand(ILI9481_CMD_MEMORY_ACCESS_CONTROL, &mac, 1);
+}
+
+void ILI9481_kbv::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+    // ILI934X just plots at edge if you try to write outside of the box:
+    if (x < 0 || y < 0 || x >= width() || y >= height())
+        return;
+    setAddrWindow(x, y, x, y);
+	spibuf[0] = (color >> 8);
+    spibuf[1] = (color >> 3);
+    spibuf[2] = (color << 3);
+	pushCommand(ILI9481_CMD_MEMORY_WRITE, spibuf, 3);
+}
+
+void ILI9481_kbv::setAddrWindow(int16_t x, int16_t y, int16_t x1, int16_t y1)
+{
+    spibuf[0] = x >> 8;
+    spibuf[1] = x;
+    spibuf[2] = x1 >> 8;
+    spibuf[3] = x1;
+	pushCommand(ILI9481_CMD_COLUMN_ADDRESS_SET, spibuf, 4);
+    spibuf[0] = y >> 8;
+    spibuf[1] = y;
+    spibuf[2] = y1 >> 8;
+    spibuf[3] = y1;
+	pushCommand(ILI9481_CMD_PAGE_ADDRESS_SET, spibuf, 4);
+}
+
+void ILI9481_kbv::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
+{
+    int16_t end;
+    if (w < 0) {
+        w = -w;
+        x -= w;
+    }                           //+ve w
+    end = x + w;
+    if (x < 0)
+        x = 0;
+    if (end > width())
+        end = width();
+    w = end - x;
+    if (h < 0) {
+        h = -h;
+        y -= h;
+    }                           //+ve h
+    end = y + h;
+    if (y < 0)
+        y = 0;
+    if (end > height())
+        end = height();
+    h = end - y;
+    setAddrWindow(x, y, x + w - 1, y + h - 1);
+    CS_ACTIVE;
+    WriteCmd(ILI9481_CMD_MEMORY_WRITE);
+    CD_DATA;
+	if (h > w) { end = h; h = w; w = end; } 
+    while (h-- > 0) {
+        write18_N(color, w);
+    }
+    CS_IDLE;
+    setAddrWindow(0, 0, width() - 1, height() - 1);
+}
+
+void ILI9481_kbv::pushColors(uint16_t * block, int16_t n, bool first)
+{
+    uint16_t color;
+    CS_ACTIVE;
+    if (first) {
+        WriteCmd(ILI9481_CMD_MEMORY_WRITE);
+    }
+    CD_DATA;
+    while (n-- > 0) {
+        color = *block++;
+        write18(color);
+    }
+    CS_IDLE;
+}
+
+void ILI9481_kbv::pushColors(const uint8_t * block, int16_t n, bool first)
+{
+    uint16_t color;
+	uint8_t h, l;
+	CS_ACTIVE;
+    if (first) {
+        WriteCmd(ILI9481_CMD_MEMORY_WRITE);
+    }
+    CD_DATA;
+    while (n-- > 0) {
+        l = pgm_read_byte(block++);
+        h = pgm_read_byte(block++);
+        color = h<<8 | l;
+		write18(color);
+    }
+    CS_IDLE;
+}
+
+void ILI9481_kbv::invertDisplay(boolean i)
+{
+    pushCommand(i ? ILI9481_CMD_DISP_INVERSION_ON : ILI9481_CMD_DISP_INVERSION_OFF, NULL, 0);
+}
+    
+void ILI9481_kbv::vertScroll(int16_t top, int16_t scrollines, int16_t offset)
+{
+    int16_t bfa = HEIGHT - top - scrollines;  // bottom fixed area
+    int16_t vsp;
+    vsp = top + offset; // vertical start position
+    if (offset < 0)
+        vsp += scrollines;          //keep in unsigned range
+    spibuf[0] = top>>8;
+	spibuf[1] = top;
+	spibuf[2] = scrollines>>8;
+	spibuf[3] = scrollines;
+	spibuf[4] = bfa>>8;
+	spibuf[5] = bfa;
+	pushCommand(0x33, spibuf, 6);
+    spibuf[0] = vsp>>8;
+	spibuf[1] = vsp;
+	pushCommand(0x37, spibuf, 2);
+}
+
+#define TFTLCD_DELAY8 0xFF
+        static const uint8_t ILI9481_RGB_regValues[] PROGMEM = {    // 320x480
+            0x01, 0,            //Soft Reset
+            TFTLCD_DELAY8, 125,
+            0x11, 0,            //Sleep Out
+            TFTLCD_DELAY8, 20,
+            0xB0, 1, 0x00,
+            0xD0, 3, 0x07, 0x41, 0x1D,  // SETPOWER [00 43 18]
+            0xD1, 3, 0x00, 0x2B, 0x1F,  // SETVCOM  [00 00 00] x0.900, x1.32
+            0xD2, 2, 0x01, 0x11,        // SETNORPOW for Normal Mode [01 22]
+            0xC0, 6, 0x00, 0x3B, 0x00, 0x02, 0x11, 0x00,     //SETPANEL [10 3B 00 02 11]
+            0xC5, 1, 0x03,      //SETOSC Frame Rate [03]
+            0xC6, 1, 0x80,      //SETRGB interface control
+			0xC8, 12, 0x00, 0x14, 0x33, 0x10, 0x00, 0x16, 0x44, 0x36, 0x77, 0x00, 0x0F, 0x00,
+            0xF3, 2, 0x40, 0x0A,			
+            0xF0, 1, 0x08,
+            0xF6, 1, 0x84,
+            0xF7, 1, 0x80,
+            0x0C, 2, 0x00, 0x55, //RDCOLMOD
+			0xB4, 1, 0x00,      //SETDISPLAY
+//			0xB3, 4, 0x00, 0x01, 0x06, 0x01,  //SETGRAM simple example
+			0xB3, 4, 0x00, 0x01, 0x06, 0x30,  //jpegs example
+            0x36, 1, 0x48,      //Memory Access [00]
+            0x3A, 1, 0x66,      //Interlace Pixel Format [XX]
+//			0x20, 0,            //INVOFF
+//			0x21, 0,            //INVON
+
+//            0x3A, 1, 0x55,      //Interlace Pixel Format [XX]
+            0x11, 0,            //Sleep Out
+            TFTLCD_DELAY8, 120,
+            0x29, 0,            //Display On
+            TFTLCD_DELAY8, 25,
+        };
+
+void ILI9481_kbv::begin(uint16_t ID)
+{
+    _lcd_ID = ID;
+	_lcd_xor = 0x02;
+    uint8_t *p = (uint8_t *)ILI9481_RGB_regValues;
+    int16_t size = sizeof(ILI9481_RGB_regValues);
+    reset();
+    while (size > 0) {
+	    uint8_t cmd = pgm_read_byte(p++);
+	    uint8_t len = pgm_read_byte(p++);
+	    if (cmd == TFTLCD_DELAY8) {
+		    delay(len);
+		    len = 0;
+		} else {
+		    CS_ACTIVE;
+		    WriteCmd(cmd);
+		    CD_DATA;
+		    for (uint8_t d = 0; d < len; d++) {
+			    uint8_t x = pgm_read_byte(p++);
+			    WriteDat8(x);
+		    }
+		    CS_IDLE;
+	    }
+	    size -= len + 2;
+    }
+    setRotation(0);             //PORTRAIT
+}
---
+++ serial9_kbv.h
@@ -0,0 +1,145 @@
+#define USE_SPICLASS
+
+#define CD_COMMAND PIN_LOW(CD_PORT, CD_PIN)
+#define CD_DATA    PIN_HIGH(CD_PORT, CD_PIN)
+#define CD_OUTPUT  PIN_OUTPUT(CD_PORT, CD_PIN)
+#define CS_ACTIVE  PIN_LOW(CS_PORT, CS_PIN);
+#define CS_IDLE    PIN_HIGH(CS_PORT, CS_PIN);
+#define CS_OUTPUT  PIN_OUTPUT(CS_PORT, CS_PIN)
+#define RESET_ACTIVE  PIN_LOW(RESET_PORT, RESET_PIN)
+#define RESET_IDLE    PIN_HIGH(RESET_PORT, RESET_PIN)
+#define RESET_OUTPUT  PIN_OUTPUT(RESET_PORT, RESET_PIN)
+#define SD_ACTIVE  PIN_LOW(SD_PORT, SD_PIN)
+#define SD_IDLE    PIN_HIGH(SD_PORT, SD_PIN)
+#define SD_OUTPUT  PIN_OUTPUT(SD_PORT, SD_PIN)
+ // bit-bang macros for SDIO
+#define SCK_LO     PIN_LOW(SPI_PORT, SCK_PIN)
+#define SCK_HI     PIN_HIGH(SPI_PORT, SCK_PIN)
+#define SCK_OUT    PIN_OUTPUT(SPI_PORT, SCK_PIN)
+#define MOSI_LO    PIN_LOW(SPI_PORT, MOSI_PIN)
+#define MOSI_HI    PIN_HIGH(SPI_PORT, MOSI_PIN)
+#define MOSI_OUT   PIN_OUTPUT(SPI_PORT, MOSI_PIN)
+#define MOSI_IN    PIN_INPUT(SPI_PORT, MOSI_PIN)
+#define LED_LO     PIN_LOW(LED_PORT, LED_PIN)
+#define LED_HI     PIN_HIGH(LED_PORT, LED_PIN)
+#define LED_OUT    PIN_OUTPUT(LED_PORT, LED_PIN)
+
+#define wait_ms(ms)  delay(ms)
+#define xchg8(x)     readbits(8);
+#define write18(x)   { write18_N(x, 1); }
+#define WriteCmd(x)  { SDIO_OUTMODE(); MOSI_LO; SCK_HI; SCK_LO; write8(x); }
+#define WriteDat8(x) { MOSI_HI; SCK_HI; SCK_LO; write8(x); }
+
+#if !defined(USE_SPICLASS)
+#include "serial_complex.h"
+#else
+#warning Using Arduino SPI methods
+
+#include <SPI.h>
+static uint8_t spibuf[16];
+
+#if 1
+#define CD_PORT PORTB
+#define CD_PIN  PB1
+#define CS_PORT PORTB
+#define CS_PIN  PB2
+#define RESET_PORT PORTB
+#define RESET_PIN  PB0
+#define SD_PORT PORTD
+#define SD_PIN  PD4
+#define SPI_PORT PORTB
+#define MOSI_PIN PB3
+#define SCK_PIN  PB5
+
+#define PIN_LOW(p, b)        (p) &= ~(1<<(b))
+#define PIN_HIGH(p, b)       (p) |= (1<<(b))
+#define PIN_OUTPUT(p, b)     *(&p-1) |= (1<<(b))
+#define PIN_INPUT(p, b)      *(&p-1) &= ~(1<<(b))
+#define PIN_READ(p, b)       (*(&p-2) & (1<<(b)))
+#else
+#define CD_PIN     9
+#define CS_PIN     10
+#define RESET_PIN  8
+#define SD_PIN     4
+#define MOSI_PIN   11
+#define SCK_PIN    13
+
+#define PIN_LOW(p, b)        digitalWrite(b, LOW)
+#define PIN_HIGH(p, b)       digitalWrite(b, HIGH)
+#define PIN_OUTPUT(p, b)     pinMode(b, OUTPUT)
+#define PIN_INPUT(p, b)      pinMode(b, INPUT_PULLUP)
+#define PIN_READ(p, b)       digitalRead(b)
+#endif
+
+#define SETDDR  { CS_OUTPUT; CD_OUTPUT; RESET_OUTPUT; PIN_HIGH(SD_PORT, SD_PIN); PIN_OUTPUT(SD_PORT, SD_PIN); }
+#define INIT()  { CS_IDLE; RESET_IDLE; SETDDR; }
+
+#define SDIO_INMODE()  MOSI_IN;SCK_OUT    //no braces
+#define SDIO_OUTMODE() {MOSI_OUT;SCK_OUT;}
+
+static inline void write8(uint8_t val)
+{
+#if 1                                   // -90.0 sec
+    SPCR = (1<<SPE)|(1<<MSTR);
+	SPSR = (1<<SPI2X);
+	SPDR = val;
+	while((SPSR & 0x80) == 0); 
+	SPCR = 0;
+#else
+    for (uint8_t i = 0; i < 8; i++) {   //send command
+        if (val & 0x80) MOSI_HI;
+	    else MOSI_LO;
+		SCK_HI;
+		SCK_LO;
+        val <<= 1;
+    }
+#endif
+}
+
+static uint32_t readbits(uint8_t bits)
+{
+	uint32_t ret = 0;
+    SDIO_INMODE();
+	while (bits--) {
+		ret <<= 1;
+		if (PIN_READ(SPI_PORT, MOSI_PIN))
+		    ret++;
+		SCK_HI;
+		SCK_LO;
+	}
+	return ret;
+}
+
+static inline void write18_N(uint16_t color, int16_t n)
+{
+#if 1                                   // -15.5 sec
+	uint8_t r = (color >> 9) | 0x80, g = (color >> 5) | 0x40, b = color | 0x20;
+	while (n-- > 0) {
+    SPCR = (1<<SPE)|(1<<MSTR);
+	SPSR = (1<<SPI2X);
+	SPDR = r;
+	while((SPSR & 0x80) == 0); 
+	SPDR = g;
+	while((SPSR & 0x80) == 0); 
+	SPDR = b;
+	while((SPSR & 0x80) == 0); 
+	SPCR = 0;
+    SCK_HI; SCK_LO;SCK_HI; SCK_LO;SCK_HI; SCK_LO;	
+	}
+#else
+	uint8_t r = color >> 8, g = (color >> 5), b = color << 3;
+	while (n-- > 0) {
+		WriteDat8(r);
+		WriteDat8(g);
+		WriteDat8(b);
+	}
+#endif
+}
+
+static inline void write9_block(uint8_t * block, int16_t n)
+{
+	while (n-- > 0) {
+		WriteDat8(*block++);
+	}
+}
+#endif
