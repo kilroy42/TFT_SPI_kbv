c82353f13113abf950ce1be611ec8398e9d7eec5
prenticedavid <david.prentice@farming.co.uk>
2016-10-20 22:18:29 +0100

    First commit of ILI9488_kbv class

---
+++ ILI9488_kbv.cpp
@@ -0,0 +1,322 @@
+#include "ILI9488_kbv.h"
+#include "serial8_kbv.h"
+
+ILI9488_kbv::ILI9488_kbv():Adafruit_GFX(320, 480)
+{
+/*
+    INIT();
+    CS_IDLE;
+    RESET_IDLE;
+*/
+}
+
+void ILI9488_kbv::reset(void)
+{
+    INIT();
+    CS_IDLE;
+    RESET_IDLE;
+	wait_ms(50);
+	RESET_ACTIVE;
+	wait_ms(100);
+	RESET_IDLE;
+	wait_ms(100);
+}
+
+void ILI9488_kbv::pushCommand(uint16_t cmd, uint8_t * block, int8_t N)
+{
+    uint16_t color;
+    CS_ACTIVE;
+    WriteCmd(cmd);
+	write9_block(block, N);
+    CS_IDLE;
+}
+
+#define ILI9488_CMD_NOP                             0x00
+#define ILI9488_CMD_SOFTWARE_RESET                  0x01
+#define ILI9488_CMD_READ_DISP_POWER                 0x0A
+#define ILI9488_CMD_READ_DISP_MADCTRL               0x0B
+#define ILI9488_CMD_READ_DISP_PIXEL_FORMAT          0x0C
+#define ILI9488_CMD_READ_DISP_IMAGE_FORMAT          0x0D
+#define ILI9488_CMD_READ_DISP_SIGNAL_MODE           0x0E
+#define ILI9488_CMD_READ_DISP_SELF_DIAGNOSTIC       0x0F
+#define ILI9488_CMD_ENTER_SLEEP_MODE                0x10
+#define ILI9488_CMD_SLEEP_OUT                       0x11
+#define ILI9488_CMD_PARTIAL_MODE_ON                 0x12
+#define ILI9488_CMD_NORMAL_DISP_MODE_ON             0x13
+#define ILI9488_CMD_DISP_INVERSION_OFF              0x20
+#define ILI9488_CMD_DISP_INVERSION_ON               0x21
+#define ILI9488_CMD_GAMMA_SET                       0x26
+#define ILI9488_CMD_DISPLAY_OFF                     0x28
+#define ILI9488_CMD_DISPLAY_ON                      0x29
+#define ILI9488_CMD_COLUMN_ADDRESS_SET              0x2A
+#define ILI9488_CMD_PAGE_ADDRESS_SET                0x2B
+#define ILI9488_CMD_MEMORY_WRITE                    0x2C
+#define ILI9488_CMD_COLOR_SET                       0x2D
+#define ILI9488_CMD_MEMORY_READ                     0x2E
+#define ILI9488_CMD_PARTIAL_AREA                    0x30
+#define ILI9488_CMD_VERT_SCROLL_DEFINITION          0x33
+#define ILI9488_CMD_TEARING_EFFECT_LINE_OFF         0x34
+#define ILI9488_CMD_TEARING_EFFECT_LINE_ON          0x35
+#define ILI9488_CMD_MEMORY_ACCESS_CONTROL           0x36
+#define ILI9488_CMD_VERT_SCROLL_START_ADDRESS       0x37
+#define ILI9488_CMD_IDLE_MODE_OFF                   0x38
+#define ILI9488_CMD_IDLE_MODE_ON                    0x39
+#define ILI9488_CMD_COLMOD_PIXEL_FORMAT_SET         0x3A
+#define ILI9488_CMD_WRITE_MEMORY_CONTINUE           0x3C
+#define ILI9488_CMD_READ_MEMORY_CONTINUE            0x3E
+#define ILI9488_CMD_SET_TEAR_SCANLINE               0x44
+#define ILI9488_CMD_GET_SCANLINE                    0x45
+
+uint16_t ILI9488_kbv::readReg(uint16_t reg)
+{
+    uint8_t h, l;
+    CS_ACTIVE;
+    WriteCmd(reg);
+    CD_DATA;                    //should do a flush()
+
+    // needs 1 dummy read
+    //     h = xchg8(0);    
+    h = xchg8(0xFF);
+    l = xchg8(0xFF);
+    CS_IDLE;
+    return (h << 8) | l;
+}
+
+uint32_t ILI9488_kbv::readReg32(uint16_t reg)
+{
+	uint32_t ret = 0;
+	CS_ACTIVE;
+	WriteCmd(reg);
+	CD_DATA;                    //should do a flush()
+    
+	readbits(1);
+    for (uint8_t cnt = 5; cnt--; ) {
+		ret <<= 8;
+		ret |= xchg8(0);
+	}
+	CS_IDLE;
+	return ret;
+}
+
+int16_t ILI9488_kbv::readGRAM(int16_t x, int16_t y, uint16_t * block, int16_t w, int16_t h)
+{
+    uint8_t r, g, b;
+	  int16_t n = w * h;    // we are NEVER going to read > 32k pixels at once
+    setAddrWindow(x, y, x + w - 1, y + h - 1);
+    CS_ACTIVE;
+    WriteCmd(ILI9488_CMD_MEMORY_READ);
+    CD_DATA;
+
+    // needs 1 dummy read
+//    r = xchg8(0xFF);
+    r = readbits(9);
+    while (n-- > 0) {
+        b = xchg8(0xFF);
+        g = xchg8(0xFF);
+        r = xchg8(0xFF);
+		*block++ = color565(r, g, b);
+    }
+    CS_IDLE;
+    setAddrWindow(0, 0, width() - 1, height() - 1);
+    return 0;
+}
+
+void ILI9488_kbv::setRotation(uint8_t r)
+{
+    uint8_t mac = 0x00;
+    Adafruit_GFX::setRotation(r & 3);
+    switch (rotation) {
+    case 0:
+        mac = 0x08;
+        break;
+    case 1:        //LANDSCAPE 90 degrees 
+        mac = 0x29;
+        break;
+    case 2:
+        mac = 0x0B;
+        break;
+    case 3:
+        mac = 0x2A;
+        break;
+    }
+	mac ^= (_lcd_xor);
+    pushCommand(ILI9488_CMD_MEMORY_ACCESS_CONTROL, &mac, 1);
+}
+
+void ILI9488_kbv::drawPixel(int16_t x, int16_t y, uint16_t color)
+{
+    // ILI934X just plots at edge if you try to write outside of the box:
+    if (x < 0 || y < 0 || x >= width() || y >= height())
+        return;
+    setAddrWindow(x, y, x, y);
+#if 1
+    CS_ACTIVE;
+	WriteCmd(ILI9488_CMD_MEMORY_WRITE);
+    CD_DATA;
+	write18_N(color, 1);
+    CS_IDLE;
+#else
+	spibuf[0] = (color >> 8);
+    spibuf[1] = (color >> 3);
+    spibuf[2] = (color << 3);
+	pushCommand(ILI9488_CMD_MEMORY_WRITE, spibuf, 3);
+#endif
+}
+
+void ILI9488_kbv::setAddrWindow(int16_t x, int16_t y, int16_t x1, int16_t y1)
+{
+    spibuf[0] = x >> 8;
+    spibuf[1] = x;
+    spibuf[2] = x1 >> 8;
+    spibuf[3] = x1;
+	pushCommand(ILI9488_CMD_COLUMN_ADDRESS_SET, spibuf, 4);
+    spibuf[0] = y >> 8;
+    spibuf[1] = y;
+    spibuf[2] = y1 >> 8;
+    spibuf[3] = y1;
+	pushCommand(ILI9488_CMD_PAGE_ADDRESS_SET, spibuf, 4);
+}
+
+void ILI9488_kbv::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
+{
+    int16_t end;
+    if (w < 0) {
+        w = -w;
+        x -= w;
+    }                           //+ve w
+    end = x + w;
+    if (x < 0)
+        x = 0;
+    if (end > width())
+        end = width();
+    w = end - x;
+    if (h < 0) {
+        h = -h;
+        y -= h;
+    }                           //+ve h
+    end = y + h;
+    if (y < 0)
+        y = 0;
+    if (end > height())
+        end = height();
+    h = end - y;
+    setAddrWindow(x, y, x + w - 1, y + h - 1);
+    CS_ACTIVE;
+    WriteCmd(ILI9488_CMD_MEMORY_WRITE);
+    CD_DATA;
+	if (h > w) { end = h; h = w; w = end; } 
+    while (h-- > 0) {
+        write18_N(color, w);
+    }
+    CS_IDLE;
+    setAddrWindow(0, 0, width() - 1, height() - 1);
+}
+
+void ILI9488_kbv::pushColors(uint16_t * block, int16_t n, bool first)
+{
+    uint16_t color;
+    CS_ACTIVE;
+    if (first) {
+        WriteCmd(ILI9488_CMD_MEMORY_WRITE);
+    }
+    CD_DATA;
+    while (n-- > 0) {
+        color = *block++;
+        write18(color);
+    }
+    CS_IDLE;
+}
+
+void ILI9488_kbv::pushColors(const uint8_t * block, int16_t n, bool first)
+{
+    uint16_t color;
+	uint8_t h, l;
+	CS_ACTIVE;
+    if (first) {
+        WriteCmd(ILI9488_CMD_MEMORY_WRITE);
+    }
+    CD_DATA;
+    while (n-- > 0) {
+        l = pgm_read_byte(block++);
+        h = pgm_read_byte(block++);
+        color = h<<8 | l;
+		write18(color);
+    }
+    CS_IDLE;
+}
+
+void ILI9488_kbv::invertDisplay(boolean i)
+{
+    pushCommand(i ? ILI9488_CMD_DISP_INVERSION_ON : ILI9488_CMD_DISP_INVERSION_OFF, NULL, 0);
+}
+    
+void ILI9488_kbv::vertScroll(int16_t top, int16_t scrollines, int16_t offset)
+{
+    int16_t bfa = HEIGHT - top - scrollines;  // bottom fixed area
+    int16_t vsp;
+    vsp = top + offset; // vertical start position
+    if (offset < 0)
+        vsp += scrollines;          //keep in unsigned range
+    spibuf[0] = top>>8;
+	spibuf[1] = top;
+	spibuf[2] = scrollines>>8;
+	spibuf[3] = scrollines;
+	spibuf[4] = bfa>>8;
+	spibuf[5] = bfa;
+	pushCommand(0x33, spibuf, 6);
+    spibuf[0] = vsp>>8;
+	spibuf[1] = vsp;
+	pushCommand(0x37, spibuf, 2);
+}
+
+#define TFTLCD_DELAY8 0xFF
+        static const uint8_t ILI9488_RGB_regValues[] PROGMEM = {    // 320x480
+            0x01, 0,            //Soft Reset
+            TFTLCD_DELAY8, 125,
+            0x11, 0,            //Sleep Out
+            TFTLCD_DELAY8, 20,
+            0x28, 0,            //Display Off
+            0xC0, 2, 0x10, 0x10,        //Power Control 1 [0E 0E]
+            0xC1, 1, 0x41,      //Power Control 2 [43]
+            0xC5, 4, 0x00, 0x22, 0x80, 0x40,    //VCOM  Control 1 [00 40 00 40]
+            0x36, 1, 0x68,      //Memory Access [00]
+            0xB0, 1, 0x00,      //Interface     [00]
+            0xB1, 2, 0xB0, 0x11,        //Frame Rate Control [B0 11]
+            0xB4, 1, 0x02,      //Inversion Control [02]
+            0xB6, 3, 0x02, 0x02, 0x3B,  // Display Function Control [02 02 3B] .kbv NL=480
+            0xB7, 1, 0xC6,      //Entry Mode      [06]
+            0xF7, 4, 0xA9, 0x51, 0x2C, 0x82,    //Adjustment Control 3 [A9 51 2C 82]
+            0x3A, 1, 0x66,      //Interlace Pixel Format [XX]
+            0x11, 0,            //Sleep Out
+            TFTLCD_DELAY8, 120,
+            0x29, 0,            //Display On
+            TFTLCD_DELAY8, 25,
+        };
+
+void ILI9488_kbv::begin(uint16_t ID)
+{
+    _lcd_ID = ID;
+	_lcd_xor = 0x02;
+    uint8_t *p = (uint8_t *)ILI9488_RGB_regValues;
+    int16_t size = sizeof(ILI9488_RGB_regValues);
+    reset();
+    while (size > 0) {
+	    uint8_t cmd = pgm_read_byte(p++);
+	    uint8_t len = pgm_read_byte(p++);
+	    if (cmd == TFTLCD_DELAY8) {
+		    delay(len);
+		    len = 0;
+		} else {
+		    CS_ACTIVE;
+		    WriteCmd(cmd);
+		    CD_DATA;
+		    for (uint8_t d = 0; d < len; d++) {
+			    uint8_t x = pgm_read_byte(p++);
+			    WriteDat8(x);
+		    }
+		    CS_IDLE;
+	    }
+	    size -= len + 2;
+    }
+    setRotation(0);             //PORTRAIT
+}
