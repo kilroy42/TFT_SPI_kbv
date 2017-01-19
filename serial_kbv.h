71630241658a38ca7b2ad427abc1c0d0db619106
prenticedavid <david.prentice@farming.co.uk>
2017-01-19 10:41:42 +0000

    support ESP8266

--- serial_kbv.h
+++ serial_kbv.h
@@ -1,4 +1,9 @@
-//#define USE_SPICLASS
+#define USE_SPICLASS
+
+#if defined(USE_SPICLASS)
+#warning Using Arduino SPI methods
+#include <SPI.h>                //include before write16() macro
+#endif
 
 #define CD_COMMAND PIN_LOW(CD_PORT, CD_PIN)
 #define CD_DATA    PIN_HIGH(CD_PORT, CD_PIN)
@@ -33,17 +38,24 @@
 #if !defined(USE_SPICLASS)
 #include "serial_complex.h"
 #else
-#warning Using Arduino SPI methods
 
-#include <SPI.h>
 static uint8_t spibuf[16];
 
+#if defined(ESP8266)
+#define CD_PIN     D9
+#define CS_PIN     D10
+#define RESET_PIN  D8
+#define SD_PIN     D4
+#define MOSI_PIN   D11
+#define SCK_PIN    D13
+#else
 #define CD_PIN     9
 #define CS_PIN     10
 #define RESET_PIN  8
 #define SD_PIN     4
 #define MOSI_PIN   11
 #define SCK_PIN    13
+#endif
 
 #define SETDDR  { CS_OUTPUT; CD_OUTPUT; RESET_OUTPUT; PIN_HIGH(SD_PORT, SD_PIN); PIN_OUTPUT(SD_PORT, SD_PIN); }
 #define INIT()  { CS_IDLE; RESET_IDLE; SETDDR; SPI.begin(); SPI.beginTransaction(settings); }
@@ -54,7 +66,7 @@ static uint8_t spibuf[16];
 #define PIN_INPUT(p, b)      pinMode(b, INPUT_PULLUP)
 #define PIN_READ(p, b)       digitalRead(b)
 
-static SPISettings settings(21000000, MSBFIRST, SPI_MODE0);
+static SPISettings settings(40000000, MSBFIRST, SPI_MODE0);
 
 static inline uint8_t xchg8_1(uint8_t x)
 {
@@ -78,15 +90,46 @@ static uint32_t readbits(uint8_t bits)
 
 static inline void write16_N(uint16_t color, int16_t n)
 {
+#if defined(ESP8266)
+    uint8_t hilo[2];
+	hilo[0] = color >> 8;
+	hilo[1] = color;
+	SPI.writePattern(hilo, 2, (uint32_t)n);
+#elif defined(BUM__SAM3X8E__)        //this is slower than 8-bit
+	while (n-- > 0) {
+		SPI.transfer16(color);
+	}
+#elif defined(TIT__SAM3X8E__)        //
+	uint16_t buf[33];
+	int16_t count = n;
+	if (n <= 0) return;
+	SPITransferMode mode = SPI_CONTINUE;
+	if (count > 32) count = 32;
+//	for (int i = 0; i < count; i++) buf[i] = color;
+	while (n > 0) {
+		n -= count;
+		if (n <= count) {
+			count = n;
+			mode = SPI_LAST;
+		}
+	    for (int i = 0; i < count; i++) buf[i] = color;
+		SPI.transfer((uint8_t*)buf, count*2, mode);
+	}
+#else
 	uint8_t hi = color >> 8, lo = color;
 	while (n-- > 0) {
 		SPI.transfer(hi);
 		SPI.transfer(lo);
 	}
+#endif
 }
 
 static inline void write8_block(uint8_t * block, int16_t n)
 {
+#if defined(ESP8266)
+	SPI.writeBytes(block, (uint32_t)n);
+#else
 	SPI.transfer(block, n);
+#endif
 }
 #endif
